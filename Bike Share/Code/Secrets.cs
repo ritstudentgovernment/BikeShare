7d0b4d9b33155ab9f562965f163062ef89c6934d
nathanCastle <nathan.castle@outlook.com>
2014-07-20 18:15:29 -0400

    Secret files
    Includes files that are necessary to support the application, with
    sensitive information excluded.

---
+++ Bike Share/Code/Secrets.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BikeShare.Code
+{
+    /// <summary>
+    /// Returns certain secret values for things like the Google Maps API key.
+    /// </summary>
+    public static class Secrets
+    {
+        /// <summary>
+        /// Returns the Google maps API key.
+        /// </summary>
+        public static string GMapsAPI
+        {
+            get
+            {
+                return "";
+            }
+        }
+    }
+}
