a87b690c983c82e50141ff42d8504d8d96466a9d
nathancastle <nathan.castle@outlook.com>
2015-04-11 19:22:37 -0400

    Fixed some configuration issues

--- Bike Share/App_Start/BundleConfig.cs
+++ Bike Share/App_Start/BundleConfig.cs
@@ -18,14 +18,8 @@ namespace BikeShare
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-          "~/Content/js/bootstrap.js",
-          "~/Scripts/respond.js"));
+          "~/Content/js/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
             bundles.Add(new LessBundle("~/Content/less").Include("~/Content/*.less"));
