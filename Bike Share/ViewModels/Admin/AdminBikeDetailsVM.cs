149a8139a5dfaf52130e8dd7665c3a67881a052b
Nathan Castle <nathan.castle@outlook.com>
2015-09-19 18:57:40 -0400

    updated packages, removed cruft from models

---
+++ Bike Share/ViewModels/Admin/AdminBikeDetailsVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShare.ViewModels.Admin
+{
+    public class AdminBikeDetailsVM : AdminBikeVM
+    {
+        public class inspection
+        {
+            public DateTime Performed { get; set; }
+            public bool Passed { get; set; }
+            public string Comment { get; set; }
+        }
+
+        public class maintenance
+        {
+            public string Title { get; set; }
+            public bool Resolved { get; set; }
+            public DateTime Date { get; set; }
+        }
+
+        public string RackLastSeen { get; set; }
+        public int CountOfInspections { get; set; }
+        public int CountOfMaintenance { get; set; }
+        public int CountOfRentals { get; set; }
+        public List<inspection> Inspections { get; set; }
+        public List<maintenance> Maintenance { get; set; }
+    }
+}
---
+++ Bike Share/ViewModels/Admin/AdminBikeVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BikeShare.ViewModels.Admin
+{
+    public class AdminBikeVM
+    {
+        [Display(Name="Archived", Description="Archived bikes are not available throughout the application. Best used for bikes that have been discarded.")]
+        public bool IsArchived { get; set; }
+
+        [Display(Name="Last Borrowed", Description="The date of the last time the bike was borrowed.")]
+        public DateTime? LastBorrowed { get; set; }
+
+        [Display(Name = "Availability", Description = "The current availability of the bike.")]
+        public bool IsAvailable { get; set; }
+
+        [Display(Description="The name of the bike. Can be used for sponsored or branded bikes.")]
+        public string Name { get; set; }
+
+        [Display(Description="The number of the bike as it is displayed on the bike.")]
+        public int Number { get; set; }
+
+        [Display(Description="The unique identifier of the bike within the website.")]
+        public int Id { get; set; }
+
+        [Display(Description="The username of the last person to borrow the bike.")]
+        public string LastCheckedOutTo { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
